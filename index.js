5e89f3a0a7abc1490daebe0368dbad92e4635a43
Muhammad Tarek <muhammadtarek@Muhammads-MacBook-Air.local>
2022-07-11 15:02:41 +0200

    deleted host from app.listen

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const app = express();
-const { PORT, HOST } = require("./startup/config");
+const { PORT } = require("./startup/config");
 
 require("./startup/db")();
 
 
 
-const server = app.listen(PORT, HOST, (err) => 
-    console.log(`Listening on host: ${HOST}\nListening on port ${PORT}`));
+const server = app.listen(PORT, (err) => 
+    console.log(`Listening on port ${PORT}`));
 
 module.exports = server;
 

5442af75fc479c91c800407a0edbe717664b9516
Muhammad Tarek <muhammadtarek@Muhammads-MacBook-Air.local>
2022-07-11 16:36:56 +0200

    another shot

--- startup/config.js
+++ startup/config.js
@@ -1,8 +1,8 @@
 const config = require("config");
 
 module.exports = {
-    PORT: config.get("PORT") || process.env.PORT || 3000,
-    NODE_ENV: config.get("NODE_ENV") || process.env.NODE_ENV || "development",
+    PORT: process.env.PORT || config.get("PORT") || 3000,
+    NODE_ENV: process.env.NODE_ENV || config.get("NODE_ENV")|| "development",
     DB_URL: config.get("DB_URL") || process.env.DB_URL || "mongodb://localhost:27017/",
     DB_NAME: config.get("DB_NAME") || process.env.DB_NAME || "",
     DB_USER: config.get("DB_USER") || process.env.DB_USER || "",
